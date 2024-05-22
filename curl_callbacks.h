4f5955ccd7390d657d8aad9146a03c30229efe09
pihnea <botoaca.mihnea@gmail.com>
2024-05-22 19:14:56 +0300

    small cosmetic change

--- curl_callbacks.h
+++ curl_callbacks.h
@@ -10,6 +10,6 @@ typedef struct {
     size_t size;
 } memory;
 
-size_t write_mem_cb(void *data, size_t size, size_t nmemb, void *clientp);
+size_t write_mem_cb(void* data, size_t size, size_t nmemb, void* clientp);
 
 #endif /* CURL_CALLBACKS_H */
